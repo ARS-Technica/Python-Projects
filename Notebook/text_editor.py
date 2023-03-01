159c6d100b77b6238a3afef4d2188ced60459577
ARS-Technica <53604048+ARS-Technica@users.noreply.github.com>
2023-02-28 23:53:41 -0500

    Update text_editor.py

--- Notebook/text_editor.py
+++ Notebook/text_editor.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Text Editor
 
@@ -31,6 +30,31 @@ my_text.pack()
 
 # Configure Scrollbar
 text_scroll.config(command=my_text.yview)
+
+# Create Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+# Add File Menu
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New")
+file_menu.add_command(label="Open")
+file_menu.add_command(label="Save")
+file_menu.add_separator()
+file_menu.add_command(label="Exit") 
+
+
+# Add Edit Menu
+edit_menu = Menu(my_menu)
+my_menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut")
+edit_menu.add_command(label="Copy")
+edit_menu.add_separator()
+edit_menu.add_command(label="Undo") 
+edit_menu.add_command(label="Redo") 
+
+
 
 
 
