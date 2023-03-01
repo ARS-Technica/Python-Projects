4e20b0db2e159dff08d734f32c45c704fbeeab5b
ARS-Technica <53604048+ARS-Technica@users.noreply.github.com>
2023-02-28 23:44:14 -0500

    Update text_editor.py

--- Notebook/text_editor.py
+++ Notebook/text_editor.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Text Editor
 
@@ -16,6 +17,20 @@ root.title('Text Editor')
 # root.iconbitmap('c:/path/to/icon.ico')
 root.geometry("1200x660")
 
+# Create Main Frame
+my_frame = Frame(root)
+my_frame.pack(pady=5)
+
+# Create Scrollbar for the Text Box
+text_scroll = Scrollbar(my_frame)
+text_scroll.pack(side=RIGHT, fill=Y)
+
+# Create Text Box
+my_text = Text(my_frame, width=97, height=25, font=("Helvetica", 16), selectbackground="yellow", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
+my_text.pack()
+
+# Configure Scrollbar
+text_scroll.config(command=my_text.yview)
 
 
 
