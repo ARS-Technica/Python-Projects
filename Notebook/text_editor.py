bcc1d88cee180b96d2e639a23f12e1f0d5e42050
ARS-Technica <53604048+ARS-Technica@users.noreply.github.com>
2023-02-28 23:35:05 -0500

    Create text_editor.py
    Create the window in Tkinter for a basic text editing desktop application.

---
+++ Notebook/text_editor.py
@@ -0,0 +1,23 @@
+"""
+Text Editor
+
+Created following a Codemy Tutorial
+
+Source: Build A Text Editor - Python Tkinter GUI Tutorial #104
+https://www.youtube.com/watch?v=UlQRXJWUNBA
+"""
+
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+
+root = Tk()
+root.title('Text Editor')
+# root.iconbitmap('c:/path/to/icon.ico')
+root.geometry("1200x660")
+
+
+
+
+
+root.mainloop()
