e9639c4e11b31f25ffd9fdf76fba7c726242bf28
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-09-21 23:07:35 -0400

    Created Trie Classes

--- Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/trie.py
+++ Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/trie.py
@@ -1,1 +1,13 @@
+# token-aware Trie
+import re
+from typing import Dict, List
 
+
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+
+class Trie:
+  pass
