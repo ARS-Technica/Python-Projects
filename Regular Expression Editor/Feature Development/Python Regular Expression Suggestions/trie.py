b3f561d15f98e5ff4af44fa801a3924e18b65e49
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-09-23 22:56:42 -0400

    Update trie.py

--- Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/trie.py
+++ Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/trie.py
@@ -4,7 +4,10 @@ from typing import Dict, List
 
 
 class TrieNode:
-    """A node in the Trie."""
+    """A Trie is a tree where each edge represents a character. 
+    Leaf nodes mark the end of a string. This structure allows 
+    us to efficiently represent shared prefixes among test cases.
+    """
 
     def __init__(self):
         self.children = {} # key: character, value: TrieNode
@@ -17,6 +20,16 @@ class Trie:
         self.root = TrieNode()
   
     pass
+
+    def insert(self, word: str):
+        """Insert a word into the Trie.""" 
+
+        pass
+
+    def build_from_list(self, words: list[str]):
+        """Build the trie from a list of words."""
+        for word in words:
+            self.insert(word)
 
 
 # -------------------------------
