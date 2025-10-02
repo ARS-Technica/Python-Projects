e4d10885011e10a69f70c3d350970a34f896a774
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-10-01 23:40:40 -0400

    Imported libraries

--- Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/gui.py
+++ Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/gui.py
@@ -1,1 +1,10 @@
+import tkinter as tk
+from tkinter import ttk, scrolledtext
+
+from regex_builder import RegExpBuilder
+from trie import Trie
+
+import re
+from regex_builder import RegExpConfig
+
 
