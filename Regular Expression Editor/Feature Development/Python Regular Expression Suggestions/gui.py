ee4ea61ea910f51093422296a8303dfcd1bcbd7c
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-10-01 23:41:36 -0400

    Created generate_button_action function

--- Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/gui.py
+++ Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/gui.py
@@ -8,3 +8,12 @@ import re
 from regex_builder import RegExpConfig
 
 
+def generate_button_action():
+    input_text = input_box.get("1.0", tk.END).strip().splitlines()
+    builder = RegExpBuilder.from_test_cases(input_text)
+ 
+    regex = builder.build()
+    output_box.config(state='normal')
+    output_box.delete("1.0", tk.END)
+    output_box.insert(tk.END, regex)
+    output_box.config(state='disabled')
