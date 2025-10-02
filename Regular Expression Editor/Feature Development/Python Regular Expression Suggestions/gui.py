6b87dd405a18e00f1cf8b90f60e5308e5c44c12c
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2025-10-01 23:42:03 -0400

    Expanded generate_button_action function

--- Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/gui.py
+++ Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/gui.py
@@ -11,7 +11,26 @@ from regex_builder import RegExpConfig
 def generate_button_action():
     input_text = input_box.get("1.0", tk.END).strip().splitlines()
     builder = RegExpBuilder.from_test_cases(input_text)
- 
+
+    if digits_var.get():
+        builder.with_conversion_of_digits()
+    if words_var.get():
+        builder.with_conversion_of_words()
+    if whitespace_var.get():
+        builder.with_conversion_of_whitespace()
+    if repetitions_var.get():
+        builder.with_conversion_of_repetitions()
+    if capturing_var.get():
+        builder.with_capturing_groups()
+    if verbose_var.get():
+        builder.with_verbose_mode()
+    if case_var.get():
+        builder.with_case_insensitive()
+    if not start_anchor_var.get():
+        builder.without_start_anchor()
+    if not end_anchor_var.get():
+        builder.without_end_anchor()
+
     regex = builder.build()
     output_box.config(state='normal')
     output_box.delete("1.0", tk.END)
