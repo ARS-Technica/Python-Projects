d841c2b3319becc498590646df4cc88f0f459227
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2026-01-12 13:21:37 -0500

    Create skeleton of generate_regex.py

---
+++ Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/skeleton of generate_regex.py
@@ -0,0 +1,6 @@
+# Cleaned-up skeleton of generate_regex
+
+# First, preprocess test_cases, tokenize them, and insert them into the trie.
+# Call to_regex on that trie.
+# trie.to_regex call comes after the trie has been filled with tokens
+
