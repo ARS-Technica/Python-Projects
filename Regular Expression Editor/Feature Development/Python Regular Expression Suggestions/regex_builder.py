cd8360426e30a5a2986fc638bef26162dcd5996b
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2026-02-04 21:30:42 -0500

    Added _tokenize_fragment Helper Function

--- Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/regex_builder.py
+++ Regular Expression Editor/Feature Development/Python Regular Expression Suggestions/regex_builder.py
@@ -185,6 +185,14 @@ class RegexConfig:
         
         return body
 
+    def _tokenize_fragment(fragment: str, is_regex: bool) -> List[str]:
+        """Tokenize a fragment into a list of tokens. Atomic regex fragments stay intact."""
+     
+        if is_regex:
+            return [fragment]
+        else:
+            return list(fragment)  # each char as token
+
 
 class RegExpBuilder:
     """
